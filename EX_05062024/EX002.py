a23aa42d0452b764dbbdf9cb5283eba92c24b3e7
aayshakazi <aayshakazi98@gmail.com>
2024-06-23 20:20:37 +0530

    Ex002 modified

---
+++ EX_05062024/EX002.py
@@ -0,0 +1,12 @@
+print("Hi", "Aaysha", "Kazi", sep="_", end="-")
+print("Hi", "Aaysha", "Kazi", end="\t ")
+print("Hi", "Aaysha", "Kazi")
+
+# SyntaxError: positional argument follows keyword argument
+# print(sep=">", "Hi", "Aaysha")
+
+print("I am Good Person",end="_")
+print("I am Bad Person")
+
+# Paython is case sensitive
+# Print("Hi")
---
+++ EX_05062024/EX003.py
@@ -0,0 +1,8 @@
+# Program to find the max in two number
+print(max(23,10))
+print(max(23,10,78))
+print(max(4,10,7, 5, 15, -9, 67.8))
+print(max(23,10, 23))
+
+# TypeError: '>' not supported between instances of 'str' and 'int'
+print(max(23,10, 23, "Aaysha"))
