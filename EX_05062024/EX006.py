a1a635a67325b1d20941988ba1806f47341ea1b3
aayshakazi <aayshakazi98@gmail.com>
2024-06-29 22:08:23 +0530

    Ex002 modified

---
+++ EX_05062024/EX006.py
@@ -0,0 +1,7 @@
+#multiple argument in one print command with diff diff ata type
+print("Hello", "World", 780, True, 3.14,)
+print("Hello", "World", 780, True, 3.14, sep="$")
+print("Hello", "World", 780, True, 3.14, sep="")
+
+# One arguement in one single qoute
+print("Hi, I am Aaysha Kazi")
---
+++ EX_05062024/EX007.py
@@ -0,0 +1,49 @@
+#Dynamically typed
+# Daynamically type - Type of data that python supprots.
+
+age= 27
+print(age)
+# variablname-variableage
+# identifier = literal
+
+# Type which supproted in the python
+# Integer - Positive and negative whole number
+# 1, -4, 56443, 01, 100000
+
+
+# Floating points numbers
+# 8.7, 3.14, -9.5 0.004, 6.00
+sgst=2.5
+print(sgst)
+
+# String - bunch of character
+# "Aaysha", "Hi", "Hi, I am Aaysha Kazi", "4533232"
+name="Aaysha"
+print(name)
+
+
+# Boolean
+# True, False
+# true, false
+
+isFemale=True
+print(isFemale)
+
+# How do I check the type  of the variable?
+print(type(age))
+print(type(sgst))
+print(type(name))
+print(type(isFemale))
+
+# Python - Complex Number  - i iota
+complex_number = 2+3j
+#Real - 2
+#Imaginary - 3
+print(complex_number.real)
+print(complex_number.imag)
+
+# Complex data type in Python
+# List
+# Tuple
+# Dictionary
+# Set
